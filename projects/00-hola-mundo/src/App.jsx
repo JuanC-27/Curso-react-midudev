5c0bf840d0b173445046cf2816a1c767c6562738
JuanC-27 <juang20133@gmail.com>
2023-01-12 12:24:39 -0500

    First Class - Creating a Twitter Card

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import TwitterFollowCard from './components/TwitterFollowCard'
+
+export default function App() {
+  // Podemos pasar a las props del componente una función
+  const formatUserName = (userName) => `@${userName}`
+  const users=[
+    {
+      name: "Juan Carlos Romero",
+      userName: "juang20133",
+      isFollowing: true
+    },
+    {
+      name: "Miguel Ángel Durán",
+      userName: "midudev",
+      isFollowing: false
+    },
+    {
+      name: "Elon Musk",
+      userName: "elonmusk",
+      isFollowing: false
+    }
+  ]  
+  return (
+    <div className='App'>
+      {
+        users.map(({userName, name, isFollowing})=>{
+          return <TwitterFollowCard 
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+            formatUserName={formatUserName}>
+            {name}
+          </TwitterFollowCard>
+        })
+      }
+    </div>
+  )
+}
---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  // <React.Fragment>
+  //   <h1>Hola Mundo</h1> {/* React.createElement("h1", null, 'Hola Mundo'), */}
+  // </React.Fragment>
+  <App />
+)
